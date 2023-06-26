e1082d1fe6bb7e21c2b126f5d2d44aac11db5dea
Bhargav Nishanth <1000072424@hexaware.com>
2023-06-26 16:15:53 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+
+if __name__== "__main__":
+    args=argparse.ArgumentParser()
+    args.add_argument("--name","-n", default="nishanth", type=str)
+    args.add_argument("--age","-a", default=32.0, type=float)
+    parse_args=args.parse_args()
+
+    print(parse_args.name,parse_args.age)
+
+
